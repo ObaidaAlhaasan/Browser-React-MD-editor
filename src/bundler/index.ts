9f3f6565fc56b46a5fbda31073b2520cd15e304c
Obaida616 <obaida.alhassan@meccasoft.com>
2021-09-22 17:47:26 +0300

    extracting bundling logic

---
+++ src/bundler/index.ts
@@ -0,0 +1,28 @@
+import { fetchPlugin } from '../plugins/fetch';
+import { unpkgPathPlugin } from '../plugins/unpkgPathPlugin';
+import * as esBuild from 'esbuild-wasm'
+
+let service: esBuild.Service;
+const bundler = async (rawCode: string) => {
+    if (!service) {
+        service = await esBuild.startService({
+            worker: true,
+            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm"
+        });
+    }
+
+    const result = await service.build({
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false,
+        plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+        define: {
+            'process.env.NODE_ENV': '"produciton"',
+            global: 'window'
+        }
+    });
+
+    return result.outputFiles[0].text;
+};
+
+export default bundler;
--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,17 @@
-import * as esBuild from 'esbuild-wasm'
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import './app.css';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom'
-import { fetchPlugin } from './plugins/fetch';
-import { unpkgPathPlugin } from './plugins/unpkgPathPlugin';
 import CodeEditor from './components/code-editor'
 import Preview from './components/preview';
+import bundler from './bundler';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import './app.css';
+
 const App = () => {
-    let ref = useRef<esBuild.Service>();
     const [input, setInput] = useState<string>('');
     const [code, setCode] = useState<string>('');
 
-    useEffect(() => {
-        startService()
-    }, []);
-
-    const startService = async () => {
-        ref.current = await esBuild.startService({
-            worker: true,
-            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm"
-        });
-
-    }
-
     const onclick = async () => {
-        if (!ref.current)
-            return;
-
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-            define: {
-                'process.env.NODE_ENV': '"produciton"',
-                global: 'window'
-            }
-        });
-
-        setCode(result.outputFiles[0].text);
+        setCode(await bundler(input));
     }
 
     return <div>
