cfa13024799c45c12dccc15494270222a3770a14
Obaida616 <obaida.alhassan@meccasoft.com>
2021-10-09 15:14:29 +0300

    setup package based development with lerna

---
+++ Sbook/packages/cli/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var local_api_1 = __importDefault(require("local-api"));
+console.log(local_api_1.default);
+(0, local_api_1.default)();
---
+++ Sbook/packages/cli/src/index.ts
@@ -0,0 +1,5 @@
+import serve from 'local-api';
+
+console.log(serve);
+
+serve();
